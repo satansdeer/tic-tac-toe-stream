f1530f42141e4c730646d16bdcf8bbce4e6089c2
Maksim Ivanov <satansdeer@gmail.com>
2020-10-21 22:15:28 +0200

    Animate transitions between screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
 import React, { useState } from "react";
+import { motion } from "framer-motion";
 import styled from "styled-components";
 import { Board, Winner } from "./Board";
 import { ResetScreen } from "./ResetScreen";
 import { StartScreen } from "./StartScreen";
 
-const BoardContainer = styled.div`
+const variants = {
+  game: {
+    opacity: 1,
+    scale: 1,
+    width: "500px",
+    height: "500px",
+    transition: { type: "spring", duration: 0.8 },
+  },
+  start: {
+    opacity: 1,
+    scale: 1,
+    width: "200px",
+    height: "100px",
+    transition: { type: "spring", duration: 0.8 },
+  },
+  reset: {
+    opacity: 1,
+    scale: 1,
+    width: "350px",
+    height: "300px",
+    transition: { type: "spring", duration: 0.8 },
+  },
+  hidden: { opacity: 0, scale: 0.8 },
+};
+
+const BoardContainer = styled(motion.div).attrs(() => ({
+  initial: "hidden",
+  variants,
+}))`
   background: #fff;
-  width: 500px;
-  height: 500px;
   border-radius: 16px;
   box-shadow: -6px 10px 30px 4px #00000033;
   border: 20px solid #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 type GameState = "start" | "game" | "reset";
@@ -20,31 +50,33 @@ function App() {
   const [gameState, setGameState] = useState<GameState>("start");
 
   const onStart = () => {
-    setGameState("game")
-  }
+    setGameState("game");
+  };
 
-  const onGameEnd = (winner:Winner) => {
-    setWinner(winner)
-    setGameState("reset")
-  }
+  const onGameEnd = (winner: Winner) => {
+    setWinner(winner);
+    setGameState("reset");
+  };
 
   const onReset = () => {
-    setWinner(undefined)
-    setGameState("game")
-  }
+    setWinner(undefined);
+    setGameState("game");
+  };
 
   return (
-    <BoardContainer>
+    <>
+    <h1>Typescript Tic-Tac-Toe</h1>
+    <BoardContainer animate={gameState}>
       {
         {
-          start: <StartScreen onStart={onStart}/>,
+          start: <StartScreen onStart={onStart} />,
           game: <Board onGameEnd={onGameEnd} />,
-          reset: <ResetScreen winner={winner} onReset={onReset}/>,
+          reset: <ResetScreen winner={winner} onReset={onReset} />,
         }[gameState]
       }
     </BoardContainer>
+    </>
   );
 }
 
 export default App;
-
--- src/StartScreen.tsx
+++ src/StartScreen.tsx
@@ -1,9 +1,19 @@
 import React, { FC } from 'react'
+import styled from 'styled-components'
+
+const StartButton = styled.button`
+  border: none;
+  font-size: 2rem;
+  cursor: pointer;
+  font-weight: bold;
+  color: #333;
+  background: none;
+`
 
 type StartScreenProps = {
   onStart(): void;
 }
 
 export const StartScreen:FC<StartScreenProps> = ({onStart}) => {
-  return <button onClick={onStart}>Start</button>
+  return <StartButton onClick={onStart}>Start</StartButton>
 }
